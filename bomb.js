0622ecb346908351d20fa73862e6336deeb115bb
emily <emilyhannan@D.local>
2018-01-26 12:04:10 -0800

    initial file setup

--- bomb.js
+++ bomb.js
@@ -4,3 +4,36 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
 });
 
+//cut wire = click = swap image out to cut wire image
+
+//can't uncut the wire, flag wire as cut
+
+//each time bomb is reset, reset 5 wired
+
+//wire has 50% chance to be cut
+
+//Cut Wires
+  //set timeout for 750 (ms) delay on a bad wire for bomb event or not bomb event
+//Reset Game
+  //reset wire logic
+  //math.random to decide which ones need to be Cut
+  //swap city image in
+//Win
+  //stop timer
+  //turn timer text green
+  //stop timeout before bomb blows p
+//Lose
+  //swap explosion image in//var
+
+//bomb
+  //keep track of bomb exploded or not exploded
+//timer
+  //stop clock when we lose = bomb
+  //should run down to 0 at the end
+
+//var timerTotal = 30 seconds
+//var timerRemain = seconds left
+//var bombDefuse = true or false
+//var bombExplode = true or false
+//var
+//var 
